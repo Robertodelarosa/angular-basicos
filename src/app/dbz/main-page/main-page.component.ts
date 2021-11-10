feaa4deaff31a139ac05000690a46afa1cef7939
Roberto De la Rosa Acosta <robertodelarosa117@gmail.com>
2021-11-09 22:01:26 -0600

    Usando el primer servicio

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,23 +1,16 @@
-import { Component, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { Personaje } from '../interfaces/dbz.interface';
-
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
 
-  personajes: Personaje[] = [
-    { nombre: 'Goku', poder: 15000 },
-    { nombre: 'Vegeta', poder: 7500 }
-  ];
   nuevo: Personaje = {
     nombre: 'Trunks',
     poder: 1000
   }
-  agregarNuevoPersonaje(nuevoPersonaje: Personaje) {
-    this.personajes.push(nuevoPersonaje)
-    console.log(this.personajes);
-  }
+
+  constructor() { }
 }
